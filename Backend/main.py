64a1ca79636a7264c3f5e1830a5ad2108284cee9
FW van der Zel <u19073862@tuks.co.za>
2023-12-08 08:42:54 +0200

    Added basic flutter framework

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+backend_app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/")
+@backend_app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@backend_app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@backend_app.get("/items/")
+async def items_from_db(skip: int = 0, limit: int = 20):
+    return fake_items_db[skip:limit]
+
